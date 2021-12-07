4a27ec8bee4d6b5eac5aa288a354d87882bc42d4
maithu89 <minhthu08092019@gmail.com>
2021-12-07 14:45:34 +0700

    thu2

---
+++ app/src/main/java/com/example/ontapkethucmon/Thuthu.java
@@ -0,0 +1,14 @@
+package com.example.ontapkethucmon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Thuthu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thuthu);
+    }
+}
